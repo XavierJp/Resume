851b542f1407b0afe8ad20308f99aad1d9cf27f8
xavier jouppe <xajouppe@gmail.com>
2025-03-23 23:17:15 +0100

    chore: setup cloudflare adapter

--- src/components/right-column/index.tsx
+++ src/components/right-column/index.tsx
@@ -3,34 +3,99 @@ import "./style.scss";
 export const RightColumn = () => {
   return (
     <div className="right-column p-4">
-      <h2>expériences professionnelles</h2>
-      <div className="block">
-        <h3 className="flex justify-content-space-between">
-          <span>Intrapreneur</span>
-          <span>2020 ∙ 2025</span>
-        </h3>
-        <div className="flex justify-content-space-between">
-          <i>Direction Intérministérielle du Numérique</i>
-          <i>Paris</i>
+      <div>
+        <h2>expériences professionnelles</h2>
+        <div className="block">
+          <h3 className="flex justify-content-space-between">
+            <span>Intrapreneur</span>
+            <span>2020 ∙ 2025</span>
+          </h3>
+          <div className="flex justify-content-space-between">
+            <i>Direction Intérministérielle du Numérique</i>
+            <i>Paris</i>
+          </div>
+          <p>
+            Gestion d’une équipe de 6 prestataires et d’un budget annuel de 350
+            000 euros, dédiés à la construction du produit Annuaire des
+            Entreprises.
+            <ul>
+              <li>
+                Organisation de l’équipe et priorisation de la feuille de route
+                et des évolutions techniques.
+              </li>
+              <li>
+                Augmentation du volume de visite jusqu’à 450 000 visites
+                mensuelles.
+              </li>
+              <li>
+                Augmentation de 150% des principales mesures d’usage et
+                d’impact.
+              </li>
+            </ul>
+          </p>
         </div>
-        <p>
-          Gestion d’une équipe de 6 prestataires et d’un budget annuel de 350
-          000 euros, dédiés à la construction du produit Annuaire des
-          Entreprises.
-          <ul>
-            <li>
-              Organisation de l’équipe et priorisation de la feuille de route et
-              des évolutions techniques.
-            </li>
-            <li>
-              Augmentation du volume de visite jusqu’à 450 000 visites
-              mensuelles.
-            </li>
-            <li>
-              Augmentation de 150% des principales mesures d’usage et d’impact.
-            </li>
-          </ul>
-        </p>
+        <div className="block">
+          <h3 className="flex justify-content-space-between">
+            <span>Intrapreneur</span>
+            <span>2020 ∙ 2025</span>
+          </h3>
+          <div className="flex justify-content-space-between">
+            <i>Direction Intérministérielle du Numérique</i>
+            <i>Paris</i>
+          </div>
+          <p>
+            Gestion d’une équipe de 6 prestataires et d’un budget annuel de 350
+            000 euros, dédiés à la construction du produit Annuaire des
+            Entreprises.
+            <ul>
+              <li>
+                Organisation de l’équipe et priorisation de la feuille de route
+                et des évolutions techniques.
+              </li>
+              <li>
+                Augmentation du volume de visite jusqu’à 450 000 visites
+                mensuelles.
+              </li>
+              <li>
+                Augmentation de 150% des principales mesures d’usage et
+                d’impact.
+              </li>
+            </ul>
+          </p>
+        </div>
+      </div>
+      <div>
+        <h2>projets et réalisations</h2>
+
+        <div className="block">
+          <h3 className="flex justify-content-space-between">
+            <span>Intrapreneur</span>
+            <span>2020 ∙ 2025</span>
+          </h3>
+          <div className="flex justify-content-space-between">
+            <i>Direction Intérministérielle du Numérique</i>
+            <i>Paris</i>
+          </div>
+          <p>
+            Gestion d’une équipe de 6 prestataires et d’un budget annuel de 350
+            000 euros, dédiés à la construction du produit Annuaire des
+            Entreprises.
+            <ul>
+              <li>
+                Organisation de l’équipe et priorisation de la feuille de route
+                et des évolutions techniques.
+              </li>
+              <li>
+                Augmentation du volume de visite jusqu’à 450 000 visites
+                mensuelles.
+              </li>
+              <li>
+                Augmentation de 150% des principales mesures d’usage et
+                d’impact.
+              </li>
+            </ul>
+          </p>
+        </div>
       </div>
     </div>
   );
