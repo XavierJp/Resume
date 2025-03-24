829dd4f2899a98c401c47616ba46d1c763223d14
xavier jouppe <xajouppe@gmail.com>
2025-03-24 12:11:44 +0100

    chore: https://github.com/withastro/astro/issues/12824 workaround

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
+import react from "@astrojs/react";
+
+// astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  output: "server",
   adapter: cloudflare(),
+  integrations: [react()],
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      //@ts-ignore
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
 });
